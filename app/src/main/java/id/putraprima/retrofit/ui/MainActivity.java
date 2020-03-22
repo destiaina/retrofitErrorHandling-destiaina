557028395120c0597c97dbcc321e99ace238609e
destiaina <ina.destia1@gmail.com>
2020-03-22 16:22:44 +0700

    retrofitget

--- app/src/main/java/id/putraprima/retrofit/ui/MainActivity.java
+++ app/src/main/java/id/putraprima/retrofit/ui/MainActivity.java
@@ -1,40 +1,27 @@
 package id.putraprima.retrofit.ui;
 
 import androidx.appcompat.app.AppCompatActivity;
-import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.View;
 import android.widget.TextView;
-import android.widget.Toast;
 import id.putraprima.retrofit.R;
-import id.putraprima.retrofit.api.helper.ServiceGenerator;
-import id.putraprima.retrofit.api.models.AppVersion;
-import id.putraprima.retrofit.api.models.LoginResponse;
-import id.putraprima.retrofit.api.services.ApiInterface;
-import retrofit2.Call;
-import retrofit2.Callback;
-import retrofit2.Response;
-import static id.putraprima.retrofit.ui.SplashActivity.getAppName;
-import static id.putraprima.retrofit.ui.SplashActivity.getAppVersion;
 
 public class MainActivity extends AppCompatActivity {
-    private static final String APP_NAME = "appName";
-    private static final String APP_VERSION = "appVersion";
-    TextView appName, appVersion;
-    private AppVersion app;
+
+    public static final String NAME_KEY="name";
+    public static final String VERSION_KEY="version";
+    TextView name, version;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        appName = findViewById(R.id.mainTxtAppName);
-        appVersion = findViewById(R.id.mainTxtAppVersion);
-        Bundle extras = getIntent().getExtras();
-        if (extras != null) {
-            // TODO: display value here
-            appName.setText(extras.getString(APP_NAME));
-            appVersion.setText(extras.getString(APP_VERSION));
+        name=findViewById(R.id.mainTxtAppName);
+        version=findViewById(R.id.mainTxtAppVersion);
+
+        Bundle extra=getIntent().getExtras();
+        if (extra!=null){
+            name.setText(extra.getString(NAME_KEY));
+            version.setText(extra.getString(VERSION_KEY));
         }
     }
 }
--- app/src/main/java/id/putraprima/retrofit/ui/SplashActivity.java
+++ app/src/main/java/id/putraprima/retrofit/ui/SplashActivity.java
@@ -8,34 +8,23 @@ import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
-import android.provider.Settings;
 import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
-import com.google.gson.Gson;
-import com.google.gson.reflect.TypeToken;
-import java.io.IOException;
-import java.lang.reflect.Type;
-import java.util.ArrayList;
-import java.util.List;
 import id.putraprima.retrofit.R;
 import id.putraprima.retrofit.api.helper.ServiceGenerator;
 import id.putraprima.retrofit.api.models.AppVersion;
-import id.putraprima.retrofit.api.models.LoginResponse;
 import id.putraprima.retrofit.api.services.ApiInterface;
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
 
 public class SplashActivity extends AppCompatActivity {
-
-    private static final String APP_NAME = "appName";
-    private static final String APP_VERSION = "appVersion";
     TextView lblAppName, lblAppTittle, lblAppVersion;
-//    private SharedPreferences preferences;
-    private ApiInterface mListener;
-    private static SharedPreferences preferences;
+    public static final String NAME_KEY="name";
+    public static final String VERSION_KEY="version";
+    private static SharedPreferences pref;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -65,42 +54,41 @@ public class SplashActivity extends AppCompatActivity {
                 = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
         NetworkInfo activeNetworkInfo = connectivityManager.getActiveNetworkInfo();
         return activeNetworkInfo != null && activeNetworkInfo.isConnected();
-        //return true;
     }
 
     private void setAppInfo() {
         //TODO : 5. Implementasikan proses setting app info, app info pada fungsi ini diambil dari shared preferences
-        //lblAppVersion dan lblAppName dimunculkan kembali dengan data dari shared preferences
-        //String ver = preferences.getString("appVersion", "value");
-        String appNa = getAppName(SplashActivity.this);
-        String appVer = getAppVersion(SplashActivity.this);
-        //String appNa = response.body().getApp();
-        lblAppName.setText(appNa);
-        lblAppVersion.setText(appVer);
-        lblAppVersion.setVisibility(View.VISIBLE);
+        String appName = getAppName(SplashActivity.this);
+        String appVersion = getAppVersion(SplashActivity.this);
+        lblAppName.setText(appName);
+        lblAppVersion.setText(appVersion);
         lblAppName.setVisibility(View.VISIBLE);
+        lblAppVersion.setVisibility(View.VISIBLE);
+        //lblAppVersion dan lblAppName dimunculkan kembali dengan data dari shared preferences
     }
 
     private void checkAppVersion() {
         ApiInterface service = ServiceGenerator.createService(ApiInterface.class);
         Call<AppVersion> call = service.getAppVersion();
         call.enqueue(new Callback<AppVersion>() {
+
             @Override
             public void onResponse(Call<AppVersion> call, Response<AppVersion> response) {
                 Toast.makeText(SplashActivity.this, response.body().getApp(), Toast.LENGTH_SHORT).show();
-//                Toast.makeText(SplashActivity.this, response.body().getVersion(), Toast.LENGTH_SHORT).show();
                 //Todo : 2. Implementasikan Proses Simpan Data Yang didapat dari Server ke SharedPreferences
-                if (response.isSuccessful()) {
-                    setAppName(SplashActivity.this, response.body().getApp());
-                    setAppVersion(SplashActivity.this, response.body().getVersion());
+                if(response.isSuccessful()){
+                    if (response.body()!=null){
+                        setAppName(SplashActivity.this, response.body().getApp());
+                        setAppVersion(SplashActivity.this, response.body().getVersion());
+                        //Todo : 3. Implementasikan Proses Pindah Ke MainActivity Jika Proses getAppVersion() sukses
+                        String appName = getAppName(SplashActivity.this);
+                        String appVersion = getAppVersion(SplashActivity.this);
+                        Intent intent = new Intent(SplashActivity.this, MainActivity.class);
+                        intent.putExtra(NAME_KEY, appName);
+                        intent.putExtra(VERSION_KEY, appVersion);
+                        startActivity(intent);
+                    }
                 }
-                //Todo : 3. Implementasikan Proses Pindah Ke MainActivity Jika Proses getAppVersion() sukses
-                String nameApp = lblAppName.getText().toString();
-                String verApp = lblAppVersion.getText().toString();
-                Intent intent = new Intent(SplashActivity.this, MainActivity.class);
-                intent.putExtra(APP_NAME, nameApp);
-                intent.putExtra(APP_VERSION, verApp);
-                startActivity(intent);
             }
 
             @Override
@@ -113,38 +101,22 @@ public class SplashActivity extends AppCompatActivity {
     }
 
     public static String getAppName(Context context) {
-        preferences = PreferenceManager.getDefaultSharedPreferences(context);
-        return preferences.getString(APP_NAME, "name");
+        pref=PreferenceManager.getDefaultSharedPreferences(context);
+        return pref.getString(NAME_KEY, "name");
     }
 
     public static void setAppName(Context context, String appName) {
-        preferences = PreferenceManager.getDefaultSharedPreferences(context);
-        preferences.edit().putString(APP_NAME, appName).apply();
+        pref=PreferenceManager.getDefaultSharedPreferences(context);
+        pref.edit().putString(NAME_KEY, appName).apply();
     }
 
     public static String getAppVersion(Context context) {
-        preferences = PreferenceManager.getDefaultSharedPreferences(context);
-        return preferences.getString(APP_VERSION, "1.0.0");
+        pref=PreferenceManager.getDefaultSharedPreferences(context);
+        return pref.getString(VERSION_KEY, "0");
     }
 
     public static void setAppVersion(Context context, String appVer) {
-        preferences = PreferenceManager.getDefaultSharedPreferences(context);
-        preferences.edit().putString(APP_VERSION, appVer).apply();
+        pref=PreferenceManager.getDefaultSharedPreferences(context);
+        pref.edit().putString(VERSION_KEY, appVer).apply();
     }
-
-//    private void login() {
-//        ApiInterface service = ServiceGenerator.createService(ApiInterface.class);
-//        Call<LoginResponse> call = service.doLogin(loginRequest);
-//        call.enqueue(new Callback<LoginResponse>() {
-//            @Override
-//            public void onResponse(Call<LoginResponse> call, Response<LoginResponse> response) {
-//                Toast.makeText(SplashActivity.this, response.body().getFirstName(), Toast.LENGTH_SHORT).show();
-//            }
-//
-//            @Override
-//            public void onFailure(Call<LoginResponse> call, Throwable t) {
-//                Toast.makeText(SplashActivity.this, "Gagal Koneksi Ke Server", Toast.LENGTH_SHORT).show();
-//            }
-//        });
-//    }
 }
